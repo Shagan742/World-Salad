749f432e9737836730c0aaa29e63793ebd1a3b16
Shagan742 <526ssangha@frhsd.com>
2024-11-16 23:55:33 +0000

    it looks better

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ let userChosenVerb=prompt('Give a verb PRESENT TENSE like runs');
 let userChosenLocation=prompt('Give a location, can be a country, state, anything');
 let userChosenGender=prompt('Give your character a gender (EX: her, his, their)');
 let userChosenSecondCharacter=prompt('Name another character');
-let userChosenSecondCharacterAdjective=prompt('Give your second character an adjective/occupation or two.');
+let userChosenSecondCharacterAdjective=prompt('Give your second character an adjective/occupation or two');
+let userChosenThirdCharacter=prompt('Give me a third character\'s name');
+let userChosenThirdCharacterAdjective=prompt('Give me an adjective/occupation for the third character');
+let userChosenSchool=prompt('Give me a school please');
 
 
 console.log(userName);
@@ -16,17 +19,23 @@ console.log(userChosenLocation);
 console.log(userChosenGender);
 console.log(userChosenSecondCharacter);
 console.log(userChosenSecondCharacterAdjective);
+console.log(userChosenThirdCharacter);
+console.log(userChosenThirdCharacterAdjective);
+console.log(userChosenSchool);
 
-
-let myStory=`hello, ${userName}! How are you doing today? How does it feel to be ${userAge} years old?`;
+let myStory=`Hello, ${userName}! How are you doing today? How does it feel to be ${userAge} years old?`;
 console.log(myStory);
 
-let myStory2=`Today I will start you a story about a kid named ${userChosenName} who ${userChosenVerb} to ${userChosenLocation}. During ${userChosenGender} visit, ${userChosenName} meets ${userChosenSecondCharacter}, a(n) ${userChosenSecondCharacterAdjective}. Now, these cards will provide separate frames to continue your story in your style.`;
+let myStory2=`Today I will start you some context. There's a kid named ${userChosenName} who ${userChosenVerb} to ${userChosenLocation}. During ${userChosenGender} visit, ${userChosenName} meets ${userChosenSecondCharacter}, a(n) ${userChosenSecondCharacterAdjective}. Now, these cards will provide separate frames to continue your story. Please have fun.`;
 console.log(myStory2);
 
 let endingOfMyStory1=`After meeting ${userChosenSecondCharacter}, ${userChosenName} and ${userChosenSecondCharacter} get along very well and end up being the bestest of friends. Together, they embark on a journey in ${userChosenLocation} about looking for the biggest mountains to exist so they can chill. However, while chilling on one mountain, ${userChosenSecondCharacter} falls off. Due to their permanent promise of always sticking together, ${userChosenName} falls off too.`;
 console.log(endingOfMyStory1);
 
+let endingOfMyStory2=`idk`;
+console.log(endingOfMyStory2);
+
 document.getElementById('story').innerHTML=myStory;
 document.getElementById('story2').innerHTML=myStory2;
 document.getElementById('endingofmyStory1').innerHTML=endingOfMyStory1;
+document.getElementById('endingofmyStory2').innerHTML=endingOfMyStory2;
